977a7d030166259d36318b3805556c1131a1deff
Kazi Muktadir Ahmed <kazisujoy@gmail.com>
2022-09-25 01:06:04 +0600

    Update AI.java

--- wumpus-world/src/LogicEngine/AI.java
+++ wumpus-world/src/LogicEngine/AI.java
@@ -2,10 +2,10 @@ package LogicEngine;
 
 public class AI {
     Cell[][] world_state = new Cell[10][10];
-    KnowledgeBase knowledgeBase;
+    KnowledgeBase knowledge_base;
 
     public AI (Cell[][] initial_world_state) {
         this.world_state = initial_world_state;
-        knowledgeBase = new KnowledgeBase(initial_world_state);
+        knowledge_base = new KnowledgeBase(initial_world_state);
     }
 }
