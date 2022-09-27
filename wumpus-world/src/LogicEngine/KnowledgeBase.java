77610b3f2200783ec3fada435698c88e7e6d29b8
Kazi Muktadir Ahmed <kazisujoy@gmail.com>
2022-09-27 15:59:51 +0600

    ok

--- wumpus-world/src/LogicEngine/KnowledgeBase.java
+++ wumpus-world/src/LogicEngine/KnowledgeBase.java
@@ -3,6 +3,7 @@ package LogicEngine;
 public class KnowledgeBase {
     Cell[][] world_state = new Cell[10][10];
 
+<<<<<<< HEAD
     public KnowledgeBase (Cell[][] initial_world_state) {
         for(int i=0; i<10; i++) {
             for (int j = 0; j < 10; j++) {
@@ -12,4 +13,6 @@ public class KnowledgeBase {
     }
 
     private void updateWorldState() {}
+=======
+>>>>>>> 7175939 (ok)
 }
