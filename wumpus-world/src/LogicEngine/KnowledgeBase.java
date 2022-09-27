6121b9dd614143d4550127b4a6fafbdf1c2a82b0
Kazi Muktadir Ahmed <kazisujoy@gmail.com>
2022-09-27 17:13:19 +0600

    added avg safety function for a cell

--- wumpus-world/src/LogicEngine/KnowledgeBase.java
+++ wumpus-world/src/LogicEngine/KnowledgeBase.java
@@ -15,18 +15,37 @@ public class KnowledgeBase {
         this.world_state[cell_row][cell_col].copyCell(new_cell);
     }
 
-    private double calculateProbability(int cell_row, int cell_col) {
+    private double calculateSafetyProbability(int cell_row, int cell_col) {
         if(world_state[cell_row][cell_col].visited) {
             if (world_state[cell_row][cell_col].safe)
-                return 0.0;
-            else
                 return 100.0;
+            else
+                return 0.0;
         }
 
         double cell_probability = 0.0;
         int count = 0;
 
-        
+        if(cell_row > 0) {
+            cell_probability += world_state[cell_row - 1][cell_col].safetyProb;
+            count++;
+        }
+
+        if (cell_row < 9) {
+            cell_probability += world_state[cell_row + 1][cell_col].safetyProb;
+            count++;
+        }
+
+        if (cell_col > 0) {
+            cell_probability += world_state[cell_row][cell_col-1].safetyProb;
+            count++;
+        }
+
+        if(cell_col < 9) {
+            cell_probability += world_state[cell_row][cell_col+1].safetyProb;
+            count++;
+        }
+        cell_probability /= count;
 
         return cell_probability;
     }
