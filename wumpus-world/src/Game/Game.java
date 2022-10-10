782f8285b0fbffd98c625784ac9ea1e6e6ffc360
Kazi Muktadir Ahmed <kazisujoy@gmail.com>
2022-10-10 12:56:11 +0600

    Update Game.java
    added updating adjacent cell function

--- wumpus-world/src/Game/Game.java
+++ wumpus-world/src/Game/Game.java
@@ -15,6 +15,29 @@ public class Game {
         String[] line;
         while (scan_file.hasNextLine()) {
             line = scan_file.nextLine().split(",");
+
+        }
+    }
+
+    public void updateAdjecentCell(int row_num, int col_num, Cell cell) {
+        if(!cell.wumpus && !cell.pit) return;
+        if(cell.wumpus || cell.pit) {
+            if(row_num > 0) {
+                game_world[row_num - 1][col_num].stench = cell.wumpus;
+                game_world[row_num - 1][col_num].breeze = cell.pit;
+            }
+            if(row_num < 9) {
+                game_world[row_num + 1][col_num].stench = cell.wumpus;
+                game_world[row_num + 1][col_num].breeze = cell.pit;
+            }
+            if(col_num > 0) {
+                game_world[row_num][col_num - 1].stench = cell.wumpus;
+                game_world[row_num][col_num - 1].breeze = cell.pit;
+            }
+            if(col_num > 0) {
+                game_world[row_num][col_num + 1].stench = cell.wumpus;
+                game_world[row_num][col_num + 1].breeze = cell.pit;
+            }
         }
     }
 }
