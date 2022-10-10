6f4f1b188bd107bcd8a78f70d0e21cb42f4033f6
Kazi Muktadir Ahmed <kazisujoy@gmail.com>
2022-10-10 12:20:13 +0600

    fixing things

--- wumpus-world/src/Game/Game.java
+++ wumpus-world/src/Game/Game.java
@@ -3,6 +3,6 @@ package Game;
 import LogicEngine.*;
 
 public class Game {
-
+    public Cell[][] game_world = new Cell[10][10];
 
 }
--- wumpus-world/src/LogicEngine/KnowledgeBase.java
+++ wumpus-world/src/LogicEngine/KnowledgeBase.java
@@ -1,23 +1,15 @@
 package LogicEngine;
 
 public class KnowledgeBase {
-    private final Cell[][] world_state = new Cell[10][10];
-
-    public KnowledgeBase(Cell[][] initial_world_state) {
-        for(int i=0; i<10; i++) {
-            for (int j = 0; j < 10; j++) {
-                this.world_state[i][j].copyCell(initial_world_state[i][j]);
-            }
-        }
-    }
+    private final Cell[][] memory = new Cell[10][10];
 
     public void updateVisitedWorldState(int cell_row, int cell_col, Cell new_cell) {
-        this.world_state[cell_row][cell_col].copyCell(new_cell);
+        this.memory[cell_row][cell_col].copyCell(new_cell);
     }
 
     private double calculateSafetyProbability(int cell_row, int cell_col) {
-        if(world_state[cell_row][cell_col].visited) {
-            if (world_state[cell_row][cell_col].safe)
+        if(memory[cell_row][cell_col].visited) {
+            if (memory[cell_row][cell_col].safe)
                 return 100.0;
             else
                 return 0.0;
@@ -27,22 +19,22 @@ public class KnowledgeBase {
         int count = 0;
 
         if(cell_row > 0) {
-            cell_probability += world_state[cell_row - 1][cell_col].safetyProb;
+            cell_probability += memory[cell_row - 1][cell_col].safetyProb;
             count++;
         }
 
         if (cell_row < 9) {
-            cell_probability += world_state[cell_row + 1][cell_col].safetyProb;
+            cell_probability += memory[cell_row + 1][cell_col].safetyProb;
             count++;
         }
 
         if (cell_col > 0) {
-            cell_probability += world_state[cell_row][cell_col-1].safetyProb;
+            cell_probability += memory[cell_row][cell_col-1].safetyProb;
             count++;
         }
 
         if(cell_col < 9) {
-            cell_probability += world_state[cell_row][cell_col+1].safetyProb;
+            cell_probability += memory[cell_row][cell_col+1].safetyProb;
             count++;
         }
         cell_probability /= count;
