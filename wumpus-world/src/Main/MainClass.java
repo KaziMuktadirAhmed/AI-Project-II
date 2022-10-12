ad09324b39b09e2ecb07729b1e5400b339c3c2a0
Kazi Muktadir Ahmed <kazisujoy@gmail.com>
2022-10-12 16:28:38 +0600

    Update MainClass.java

--- wumpus-world/src/Main/MainClass.java
+++ wumpus-world/src/Main/MainClass.java
@@ -6,13 +6,12 @@ import java.io.IOException;
 
 public class MainClass {
     public static void main(String[] args) throws IOException {
-//        System.out.println("hello cruel world!!!");
         test();
     }
 
     public static void test() throws IOException {
         Game game = new Game();
-//        game.printWorld();
+        game.printWorld();
         game.run();
     }
 }
